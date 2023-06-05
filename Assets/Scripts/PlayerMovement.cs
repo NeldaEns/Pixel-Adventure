ba2dcedaeefad0f2fe786cf5c60c6f55f6edf3c1
NeldaEns <sang82168@gmail.com>
2023-06-05 10:04:43 +0700

    start

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D rb;
+    private Animator anim;
+    private SpriteRenderer sprite;  
+    private float dirX = 0f;
+    private float moveSpeed = 7f;
+    private float jumpForce = 14f;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+    private void Update()
+    {
+         dirX = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector3(dirX * 7f, rb.velocity.y, 0);
+         if(Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector3(rb.velocity.x, 14f, 0);
+        }
+        UpdateAnimation();
+        
+    }
+
+    private void UpdateAnimation()
+    {
+        if (dirX > 0f)
+        {
+            anim.SetBool("running", true);
+            sprite.flipX = false;
+        }
+        else if (dirX < 0f)
+        {
+            anim.SetBool("running", true);
+            sprite.flipX = true;
+        }
+        else
+        {
+            anim.SetBool("running", false);
+        }
+    }
+}
