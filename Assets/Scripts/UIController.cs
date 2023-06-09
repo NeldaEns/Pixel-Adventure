15ff83c3e96b3ae8b880fba2f544f7972b310b5a
NeldaEns <sang82168@gmail.com>
2023-06-09 17:34:47 +0700

    level

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    public static UIController ins;
+    [HideInInspector]
+    public UIScreenBase currentScreen;
+    public GameObject mainMenu;
+    public GameObject maincameraPrefab;
+
+
+    private void Awake()
+    {
+        GameObject mainCamera = Instantiate(maincameraPrefab);
+        if(ins != null)
+        {
+            Destroy(gameObject);
+            Destroy(mainCamera);
+        }
+        else
+        {
+            ins = this;
+            DontDestroyOnLoad(gameObject);
+            DontDestroyOnLoad(mainCamera);
+        }
+    }
+
+    private void Start()
+    {
+        Canvas canvas = gameObject.GetComponent<Canvas>();
+        canvas.renderMode = RenderMode.ScreenSpaceCamera;
+        canvas.worldCamera = Camera.main;
+        currentScreen = Instantiate(mainMenu, transform).GetComponent<MenuUI>();
+    }
+
+    public void ShowMenu()
+    {
+        Destroy(currentScreen.gameObject);
+        currentScreen = Instantiate(mainMenu, transform).GetComponent<MenuUI>();
+    }
+}
